11d9c90da37a376a32d21b5c03cda7e605c664bb
Gholamreza Hassanzadeh Tabrizi <gholamreza.hassanzade.1994@gmail.com>
2025-06-04 13:27:04 +0330

    feat all products images

--- src/modules/ProductList.tsx
+++ src/modules/ProductList.tsx
@@ -1,29 +1,42 @@
-import { Link } from '@tanstack/react-router';
-import type { Product } from '@/types/product';
-import { DataTable } from '../components/template/dataTable/DataTable';
-import { Badge } from '../components/atoms/Badge';
-import { ProductRatingIcon } from '../components/atoms/ProductRatingIcon';
-import type { ColumnDef } from '@tanstack/react-table';
+import { Link } from "@tanstack/react-router";
+import type { Product } from "@/types/product";
+import { DataTable } from "../components/template/dataTable/DataTable";
+import { Badge } from "../components/atoms/Badge";
+import { ProductRatingIcon } from "../components/atoms/ProductRatingIcon";
+import type { ColumnDef } from "@tanstack/react-table";
 
 export const ProductList = () => {
   const columns: ColumnDef<Product>[] = [
     {
-      header: 'تصویر',
-      accessorKey: 'thumbnail',
+      header: "تصویر",
+      accessorKey: "thumbnail",
       size: 100,
-      cell: ({ row }) => (
-        <div className="w-16 h-16 rounded-lg overflow-hidden">
-          <img
-            src={row.original.thumbnail}
-            alt={row.original.title}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      ),
+      cell: ({ row }) => {
+        const rating = row.original.rating || 0;
+        let imageIndex = 0;
+        if (rating < 3) {
+          imageIndex = 0;
+        } else if (rating >= 3 && rating <= 4) {
+          imageIndex = 1;
+        } else {
+          imageIndex = 2;
+        }
+        const imageSrc =
+          row.original.images?.[imageIndex] || row.original.thumbnail;
+        return (
+          <div className="w-8 h-8 rounded-lg overflow-hidden">
+            <img
+              src={imageSrc}
+              alt={row.original.title}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        );
+      },
     },
     {
-      header: 'نام محصول',
-      accessorKey: 'title',
+      header: "نام محصول",
+      accessorKey: "title",
       size: 200,
       cell: ({ row }) => (
         <div className="flex flex-col gap-1">
@@ -39,8 +52,8 @@ export const ProductList = () => {
       ),
     },
     {
-      header: 'امتیاز',
-      accessorKey: 'rating',
+      header: "امتیاز",
+      accessorKey: "rating",
       size: 150,
       cell: ({ row }) => (
         <div className="flex items-center gap-2">
@@ -49,12 +62,14 @@ export const ProductList = () => {
       ),
     },
     {
-      header: 'قیمت',
-      accessorKey: 'price',
+      header: "قیمت",
+      accessorKey: "price",
       size: 150,
       cell: ({ row }) => (
         <div className="flex items-center space-x-2">
-          <span className="font-semibold text-xs">${row.original.price.toFixed(2)}</span>
+          <span className="font-semibold text-xs">
+            ${row.original.price.toFixed(2)}
+          </span>
           {row.original.discountPercentage > 0 && (
             <span className="text-green-600 text-xs">
               {row.original.discountPercentage}% تخفیف
@@ -64,23 +79,25 @@ export const ProductList = () => {
       ),
     },
     {
-      header: 'دسته‌بندی',
-      accessorKey: 'category',
+      header: "دسته‌بندی",
+      accessorKey: "category",
       size: 150,
       cell: ({ row }) => (
         <span className="text-gray-500 text-xs">{row.original.category}</span>
       ),
     },
     {
-      header: 'وضعیت',
-      accessorKey: 'availabilityStatus',
+      header: "وضعیت",
+      accessorKey: "availabilityStatus",
       size: 150,
       cell: ({ row }) => (
         <Badge
           variant="text"
-          color={row.original.availabilityStatus === 'In Stock' ? 'success' : 'error'}
+          color={
+            row.original.availabilityStatus === "In Stock" ? "success" : "error"
+          }
         >
-          {row.original.availabilityStatus === 'In Stock' ? 'موجود' : 'ناموجود'}
+          {row.original.availabilityStatus === "In Stock" ? "موجود" : "ناموجود"}
         </Badge>
       ),
     },
@@ -104,4 +121,4 @@ export const ProductList = () => {
       />
     </div>
   );
-}; 
+};
