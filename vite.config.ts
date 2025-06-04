c2b428b429b94b4fa34645df68ff05137a770ac3
Gholamreza Hassanzadeh Tabrizi <gholamreza.hassanzade.1994@gmail.com>
2025-06-04 10:17:58 +0330

    feat alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "./src"),
+      },
+      {
+        find: "@/assets",
+        replacement: resolve(__dirname, "./src/assets"),
+      },
+      {
+        find: "@/components",
+        replacement: resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "@/configurations",
+        replacement: resolve(__dirname, "./src/configurations"),
+      },
+      {
+        find: "@/constants",
+        replacement: resolve(__dirname, "./src/constants"),
+      },
+      {
+        find: "@/services",
+        replacement: resolve(__dirname, "./src/services"),
+      },
+      {
+        find: "@/modules",
+        replacement: resolve(__dirname, "./src/modules"),
+      },
+      {
+        find: "@/routers",
+        replacement: resolve(__dirname, "./src/routers"),
+      },
+    ],
   },
 })
