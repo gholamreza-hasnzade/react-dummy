eef97f4de239c568ae09b2a86a5d68483e540c44
Gholamreza Hassanzadeh Tabrizi <gholamreza.hassanzade.1994@gmail.com>
2025-06-05 13:10:12 +0330

    feat buges

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import "./index.css";
 import { AppConfigurations } from "@/configurations/app.configurations";
 import { App } from "./App.tsx";
+import "./index.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AppConfigurations>
-        <App />
-      </AppConfigurations>
-    </QueryClientProvider>
+    <AppConfigurations>
+      <App />
+    </AppConfigurations>
   </StrictMode>
 );
